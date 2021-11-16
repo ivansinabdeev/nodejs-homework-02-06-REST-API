46bb76f7616c6bc71c98503466ee4e6454456f54
ivansinabdeev <ivansinabdeev@gmail.com>
2021-11-16 21:34:01 +0100

    fixed and done tast :ant:

--- model/index.js
+++ model/index.js
@@ -17,7 +17,7 @@ const updateContacts = async (newContacts) => {
 
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
-  const [result] = contacts.filter(
+  const result = contacts.find(
     (contact) => String(contact.id) === String(contactId)
   );
   if (!result) {
@@ -36,7 +36,7 @@ const addContact = async (name, email, phone) => {
 
 const updateContact = async (contactId, body) => {
   const contacts = await listContacts();
-  const [result] = contacts.filter(
+  const result = contacts.findIndex(
     (contact) => String(contact.id) === String(contactId)
   );
   if (!result) {
